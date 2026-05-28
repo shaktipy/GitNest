597fc53563423b52f7611fc17e3bf0b428c81e2c
Vrajkumar Shah <vrajshah826@gmail.com>
2026-05-28 17:46:20 +0530

    feat: implement saga orchestration and event-driven consistency system

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,6 +21,7 @@ import swaggerSpec from './config/swagger.js';
 import { requestIdMiddleware, attachRequestIdToResponse } from './middleware/requestId.js';
 import { sendError } from './utils/responseHandlers.js';
 import ERROR_CODES from './constants/errorCodes.js';
+import './events/subscribers.js';
 
 const createApp = () => {
   const app = express();
